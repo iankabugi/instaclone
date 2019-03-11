074a727c6209cb5d6f0b182f118748f793ccaa20
iankabugi <iank299@gmail.com>
2019-03-11 14:54:26 +0300

    add an image model with most the properties needed

--- insta/models.py
+++ insta/models.py
@@ -2,40 +2,125 @@ from django.db import models
 import datetime as dt
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
+import pyperclip
+# Create your models here.
 
 
-class tags(models.Model):
+class Location(models.Model):
+    """
+    This is the class where we will create locations
+    """
     name = models.CharField(max_length=30)
+
+    def save_location(self):
+        """
+        This is the function that we will use to save the instance of this class
+        """
+        self.save()
+
+    def delete_location(self):
+        """
+        This is the method to delete the instance
+        """
+        self.delete()
+
+    def update_location(self, field, val):
+        """
+        This is the method to update the instance
+        """
+        Location.objects.get(id=self.id).update(field=val)
 
     def __str__(self):
         return self.name
 
 
-class Article(models.Model):
-    title = models.CharField(max_length=60)
-    post = HTMLField()
-    editor = models.ForeignKey(User, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(tags)
-    pub_date = models.DateTimeField(auto_now_add=True)
-    article_image = models.ImageField(upload_to='articles/', blank=True)
-
-    @classmethod
-    def todays_news(cls):
-        today = dt.date.today()
-        news = cls.objects.filter(pub_date__date=today)
-        return news
-
-    @classmethod
-    def days_news(cls, date):
-        news = cls.objects.filter(pub_date__date=date)
-        return news
-
-    @classmethod
-    def search_by_title(cls, search_term):
-        news = cls.objects.filter(title__icontains=search_term)
-        return news
-
-
-class NewsLetterRecipients(models.Model):
+class Category(models.Model):
+    """
+    This is the class where we will create categories
+    """
     name = models.CharField(max_length=30)
-    email = models.EmailField()
+
+    def save_category(self):
+        """
+        This is the function that we will use to save the instance of this class
+        """
+        self.save()
+
+    def delete_category(self):
+        """
+        This is the method to delete the instance
+        """
+        Category.objects.get(id=self.id).delete()
+
+    def update_category(self, field, val):
+        """
+        This is the method to update the instance
+        """
+        Category.objects.get(id=self.id).update(field=val)
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    image= models.ImageField(upload_to="images/")
+    name = models.CharField(max_length=30)
+    caption = models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def save_image(self):
+        """
+        The function that we will use to save the instance of this class
+        """
+        self.save()
+
+    def delete_image(self):
+        """
+        The function that we will use to delete the instance of this class
+        """
+        Image.objects.get(id=self.id).delete()
+
+    def update_image(self, val):
+        """
+        The method to update the instance
+        """
+        Image.objects.filter(id=self.id).update(name=val)
+
+    @classmethod
+    def get_image_by_id(cls, image_id):
+        """
+        The method to get a specific image
+        """
+        return cls.objects.get(id=image_id)
+
+    @classmethod
+    def copy_image(image):
+        find_image = Image.get_image_by_id(image_id)
+        return pyperclip.copy(find_image.image)
+
+    @classmethod
+    def show_image(cls, category):
+        images = cls.objects.filter(category_name=category)
+        return images
+
+    @classmethod
+    def get_photos(cls):
+        return cls.objects.all()
+
+    @classmethod
+    def search_by_category(cls, category):
+        photo = Category.objects.filter(name_icontains=category)[0]
+        return cls.objects.filter(category_id=photo.id)
+
+    @classmethod
+    def filter_by_location(cls, location):
+        """
+        The method to get images taken in a certain location
+        """
+        the_location = Location.objects.get(name=location)
+        return cls.objects.filter(location_id=the_location.id)
+
+    def __str__(self):
+        return self.name
