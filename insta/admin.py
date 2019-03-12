b9dd07129a8539e1a0fae4815c791e50adc64f2f
iankabugi <iank299@gmail.com>
2019-03-12 17:37:52 +0300

    register your models to admin.py

--- insta/admin.py
+++ insta/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Location,Profile,Image
+# Register your models here.
+# image details
+admin.site.register(Location)
+admin.site.register(Image)
+admin.site.register(Profile)
 # Register your models here.
