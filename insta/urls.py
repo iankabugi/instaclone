fc1d34c932bcdc440884c6e3b27fc16bea844d63
iankabugi <iank299@gmail.com>
2019-03-12 17:39:12 +0300

    define routes for the app

--- insta/urls.py
+++ insta/urls.py
@@ -4,7 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url(r'^$',views.welcome,name='welcome'),
+    url('^$', views.photos, name='photos'),
+    url(r'^search/', views.search_results, name='search_results'),
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
